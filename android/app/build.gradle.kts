1d07b8adcc1b234cb45ec0fc1eb4545685c5cf47
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2025-09-29 06:53:24 +0300

    Update version scheme (#3)
    * Update version scheme
    
    * Fixed gradle for non release
    
    * no pr flavor
    
    * code rabbit suggestions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.*
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -21,7 +24,6 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.trigon.scoutingapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
@@ -31,11 +33,40 @@ android {
         minSdk = flutter.minSdkVersion
     }
 
+    // Load key.properties if it exists
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    val keystoreProperties = Properties()
+    if (keystorePropertiesFile.exists()) {
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+    }
+
+    signingConfigs {
+        create("release") {
+            // Only assign properties if file exists
+            if (keystorePropertiesFile.exists()) {
+                (keystoreProperties.getProperty("keyAlias") ?: "").takeIf { it.isNotEmpty() }?.let { keyAlias = it }
+                (keystoreProperties.getProperty("keyPassword") ?: "").takeIf { it.isNotEmpty() }
+                    ?.let { keyPassword = it }
+                keystoreProperties.getProperty("storeFile")?.takeIf { it.isNotEmpty() }?.let { storeFile = file(it) }
+                (keystoreProperties.getProperty("storePassword") ?: "").takeIf { it.isNotEmpty() }
+                    ?.let { storePassword = it }
+            } else {
+                storeFile = null
+            }
+        }
+    }
+
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            if (keystorePropertiesFile.exists()) signingConfig = signingConfigs.getByName("release")
+        }
+        create("pr") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            isDebuggable = true
+            signingConfig = null
         }
     }
 }
